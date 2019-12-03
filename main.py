9fcd94da2c2ee05553075bb3159c6ef52febdab7
RemoteRacoon <remote_racoon@mail.ru>
2019-12-03 10:30:36 +0800

    Lama has been found!

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from skimage import filters, measure
-from skimage.measure import label
+import matplotlib.pyplot as plt
+from skimage.measure import label, regionprops
+from scipy.ndimage import binary_dilation
+from skimage.filters import threshold_otsu
+from skimage import filters
 
-# воходное изображение нужно избавить от 
-# рамки, это связано с расчетом производной
-img = plt.imread("./lama_on_moon.png")
-gray = np.average(img, 2)[50:-50, 50:-50]
+image = plt.imread("./lama_on_moon.png")
+gray = np.average(image, 2)[50:-50, 50:-50]
 
 sobel = filters.sobel(gray)
 thresh = filters.threshold_otsu(sobel)
 sobel[sobel < thresh] = 0
 sobel[sobel >= thresh] = 1
 
-max_area = 0
-index = -1
-LB = measure.label(sobel)
-props = measure.regionprops(LB)
+lb = label(sobel)
+symbols = regionprops(lb)
+areas=[]
+for sym in symbols:
+    areas.append(sym.area)
 
-for prop in enumerate(props):
-    if prop.convex_area > max_area:
-        index = i
-        max_area = prop.convex_area
+maxi=0
+i=0
 
-bb = props[index].bbox
-lama = img[bb[0]::bb[2], bb[1]::bb[3]]
+for ar in areas:
+    i=i+1
+    if ar>maxi:
+        maxi=ar
+        j=i
+        x1 , y1, x2, y2 = symbols[j-1].bbox
 
-plt.subplot(111)
-plt.imshow(sobel)
+image1 = image[x1+50:x2+50, y1+50:y2+50]
+
+plt.figure()
+plt.imshow(image1)
 plt.show()
