5c121cb93ead3a0dea0f62b52338265f5947dabe
RemoteRacoon <remote_racoon@mail.ru>
2019-12-03 10:51:51 +0800

    ball image added

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+# to count all balls, we have to 
+# count how many balls belong to 
+# a certain color
 import matplotlib.pyplot as plt 
 import numpy as np 
 from skimage import filters, measure
 
 image = plt.imread("./circ1.png")[:,:,:]
 
-LB = measure.label(image)
-props = measure.regionprops(LB)
-length = len(props)
-print(length)
+result = []
+for i in range(image.shape[-1]):
+    lb = measure.label(image[:, :, i])
+    result.append(np.max(lb))
 
-
+print(result)
+print(np.sum(result[:-1]))
 plt.figure(figsize=(8,8))
 plt.imshow(image)
 plt.show()
